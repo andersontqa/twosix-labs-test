3bf4b619dc286ecb2435cd72a25f478a7f9bef7a
Steezy <andersont.qa@gmail.com>
2020-01-23 22:10:28 -0500

    Added functionality of covering both author names and scrolling to bottom one

--- two_six_labs_test.py
+++ two_six_labs_test.py
@@ -11,14 +11,26 @@ class TestTwoSixLabs:
 		search.submit()
 		first_item = self.driver.find_element_by_css_selector("#rso > div:nth-child(1) > div > div > div > div > div > div:nth-child(3) > div > div > div > a")
 		first_item.click()
-		author_name = self.driver.find_element_by_css_selector("#f0mcELYnsjf5vr > div > div.m-byline__meta.a-body1 > div.m-byline__author > span > span > a").text
+		author_name_top = self.driver.find_element_by_css_selector("#f0mcELYnsjf5vr > div > div.m-byline__meta.a-body1 > div.m-byline__author > span > span > a").text
+		author_name_bottom = self.driver.find_element_by_css_selector("#f0XdmP5nsjf5vr > div > h6 > a").text
 		try:
-			assert author_name == "Mark Pomerleau"
-			self.driver.save_screenshot("correct_author_name.png")
+			assert author_name_top == "Mark Pomerleau"
+			self.driver.save_screenshot("correct_author_name_top.png")
 		except AssertionError:
-			print(author_name + " is not Mark Pomerleau")
-			self.driver.save_screenshot("wrong_author_name.png")
+			print(author_name_top + " is not Mark Pomerleau")
+			self.driver.save_screenshot("wrong_author_name_top.png")
 			assert False
+
+		scroll = self.driver.execute_script("window.scrollTo(0, 5093)")
+		
+		try:
+			assert author_name_bottom == "Mark Pomerleau"
+			self.driver.save_screenshot("correct_author_name_bottom.png")
+		except AssertionError:
+			print(author_name_bottom + " is not Mark Pomerleau")
+			self.driver.save_screenshot("wrong_author_name_bottom.png")
+			assert False
+
 
 
 		
